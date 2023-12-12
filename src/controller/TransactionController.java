e1c8e12d6d342dffe4ac1e4cee917941d188dec4
JonathanAaron3005 <aaron.wibawa@gmail.com>
2023-12-12 19:14:29 +0700

    add transaction logic

--- src/controller/TransactionController.java
+++ src/controller/TransactionController.java
@@ -33,45 +33,117 @@ public class TransactionController {
 //		}
 //	}
 	
-	public void AddTransactionHeader(Integer StaffID, Date transactionDate) {
-		TransactionHeader th = new TransactionHeader();
-		UserController uc = new UserController();
-		String StaffName = uc.GetName(StaffID);
-		th.setStaffID(StaffID);
-		th.setStaffName(StaffName);
-		th.setTransactionDate(transactionDate);
-		String query = "INSERT INTO TransactionHeader(StaffID, StaffName, TransactionDate) VALUES (?, ?, ?, ?)";
-		try(Connection connection = Database.getDB().getConnection()){
-			PreparedStatement ps = connection.prepareStatement(query);
-			ps.setInt(1, th.getStaffID());
-			ps.setString(1, th.getStaffName());
-			ps.setDate(1, th.getTransactionDate());
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+//	public void AddTransactionHeader(Integer StaffID, Date transactionDate) {
+//		TransactionHeader th = new TransactionHeader();
+//		UserController uc = new UserController();
+//		String StaffName = uc.GetName(StaffID);
+//		th.setStaffID(StaffID);
+//		th.setStaffName(StaffName);
+//		th.setTransactionDate(transactionDate);
+//		String query = "INSERT INTO TransactionHeader(StaffID, StaffName, TransactionDate) VALUES (?, ?, ?, ?)";
+//		try(Connection connection = Database.getDB().getConnection()){
+//			PreparedStatement ps = connection.prepareStatement(query);
+//			ps.setInt(1, th.getStaffID());
+//			ps.setString(1, th.getStaffName());
+//			ps.setDate(1, th.getTransactionDate());
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	public void AddTransactionDetail(Integer TransactionID, ArrayList<PCBook> PcBook) {
+//		for(int i=0;i<PcBook.size();i++) {
+//			TransactionDetail td = new TransactionDetail();
+//			String queryDetail = "INSERT INTO TransactionDetail(TransactionID, PC_ID, CustomerName, BookedTime) VALUES (?, ?, ?, ?)";
+//			PCBookController uc = new PCBookController();
+//			td.setTransactionID(TransactionID);
+////			Integer PC_ID = ;
+////			th.setStaffID(StaffID);
+////			th.setStaffName(StaffName);
+////			th.setTransactionDate(transactionDate);
+////			String query = "INSERT INTO TransactionHeader(StaffID, StaffName, TransactionDate) VALUES (?, ?, ?, ?)";
+////			try(Connection connection = Database.getDB().getConnection()){
+////				PreparedStatement ps = connection.prepareStatement(query);
+////				ps.setInt(1, th.getStaffID());
+////				ps.setString(1, th.getStaffName());
+////				ps.setDate(1, th.getTransactionDate());
+////			} catch (SQLException e) {
+////				e.printStackTrace();
+////			}
+//		}
+//	}
 	
-	public void AddTransactionDetail(Integer TransactionID, ArrayList<PCBook> PcBook) {
-		for(int i=0;i<PcBook.size();i++) {
-			TransactionDetail td = new TransactionDetail();
-			String queryDetail = "INSERT INTO TransactionDetail(TransactionID, PC_ID, CustomerName, BookedTime) VALUES (?, ?, ?, ?)";
-			PCBookController uc = new PCBookController();
-			td.setTransactionID(TransactionID);
-//			Integer PC_ID = ;
-//			th.setStaffID(StaffID);
-//			th.setStaffName(StaffName);
-//			th.setTransactionDate(transactionDate);
-//			String query = "INSERT INTO TransactionHeader(StaffID, StaffName, TransactionDate) VALUES (?, ?, ?, ?)";
-//			try(Connection connection = Database.getDB().getConnection()){
-//				PreparedStatement ps = connection.prepareStatement(query);
-//				ps.setInt(1, th.getStaffID());
-//				ps.setString(1, th.getStaffName());
-//				ps.setDate(1, th.getTransactionDate());
-//			} catch (SQLException e) {
-//				e.printStackTrace();
-//			}
-		}
-	}
+	public void addTransaction(Integer staffID, Date transactionDate, ArrayList<PCBook> pcBooks) {
+        TransactionHeader header = addTransactionHeader(staffID, transactionDate);
+        if (header != null) {
+            int transactionID = header.getTransactionID();
+            addTransactionDetails(transactionID, pcBooks);
+        }
+    }
+
+    private TransactionHeader addTransactionHeader(Integer staffID, Date transactionDate) {
+        TransactionHeader header = new TransactionHeader();
+        UserController uc = new UserController();
+        
+        String staffName = uc.GetName(staffID);
+        header.setStaffID(staffID);
+        header.setStaffName(staffName);
+        header.setTransactionDate(transactionDate);
+
+        String query = "INSERT INTO TransactionHeader(StaffID, StaffName, TransactionDate) VALUES (?, ?, ?)";
+        try (Connection connection = Database.getDB().getConnection();
+             PreparedStatement ps = connection.prepareStatement(query)) {
+
+            ps.setInt(1, header.getStaffID());
+            ps.setString(2, header.getStaffName());
+            ps.setDate(3, new java.sql.Date(header.getTransactionDate().getTime()));
+
+            int rowsAffected = ps.executeUpdate();
+
+            if (rowsAffected > 0) {
+                return header; 
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    
+    private void addTransactionDetails(Integer transactionID, ArrayList<PCBook> pcBooks) {
+        String query = "INSERT INTO TransactionDetail(TransactionID, PC_ID, CustomerName, BookedTime) VALUES (?, ?, ?, ?)";
+        try (Connection connection = Database.getDB().getConnection();
+             PreparedStatement ps = connection.prepareStatement(query)) {
+
+            for (PCBook pb : pcBooks) {
+                TransactionDetail detail = new TransactionDetail();
+                detail.setTransactionID(transactionID);
+                detail.setPC_ID(pb.getPC_ID());
+
+                UserController uc = new UserController();
+                String customerName = uc.GetName(pb.getUserID());
+                detail.setCustomerName(customerName);
+
+                detail.setBookedTime(pb.getBookedDate().toString()); 
+
+                ps.setInt(1, detail.getTransactionID());
+                ps.setInt(2, detail.getPC_ID());
+                ps.setString(3, detail.getCustomerName());
+                ps.setString(4, detail.getBookedTime());
+                ps.addBatch(); 
+            }
+            int[] affectedRows = ps.executeBatch();
+            for (int row : affectedRows) {
+                if (row <= 0) {
+                    throw new SQLException("One or more statements in the batch failed to execute.");
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 	public ArrayList<TransactionHeader> GetAllTransactionHeader() {
 		ArrayList<TransactionHeader> th = new ArrayList<TransactionHeader>();
